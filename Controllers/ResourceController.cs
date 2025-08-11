ca3d4fdf97e2fc36633412ccfbee6751b711283a
Naing Linn Aung <naing@evantek.com>
2025-08-11 09:44:30 +0800

    'initial'

---
+++ Controllers/ResourceController.cs
@@ -0,0 +1,108 @@
+﻿using AuthServerNew.Data;
+using Bogus;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OAuthServer.Models;
+using OpenIddict.Abstractions;
+using OpenIddict.Validation.AspNetCore;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
+namespace OAuthServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
+    public class ResourceController : Controller
+    {
+        public readonly IConfiguration _configuration;
+        public ResourceController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet("message")]
+        public IActionResult GetMessage()
+        {
+            var clientId = User.GetClaim(Claims.Subject);
+            return Ok($"Client '{clientId}' is authenticated.");
+        }
+
+        //Header validation for this endpoint
+        [HttpPost("searchCustomerProfileByFilter")]
+        public IActionResult SearchCustomerProfileByFilter([FromBody] ParaCustomerProfile _paylod) 
+        {
+            #region header validation
+            if (!Request.Headers.TryGetValue("accept", out var accept))
+            {
+                return BadRequest("Missing accept header.");
+            }
+
+            if (!Request.Headers.TryGetValue("X-DBS-ReqUID", out var  request_uid))
+            {
+                return BadRequest("Missing X-DBS-ReqUID header.");
+            }
+
+            if (!Request.Headers.TryGetValue("X-DBS-ReqClientId", out var req_client_id))
+            {
+                return BadRequest("Missing X-DBS-ReqClientId header.");
+            }
+
+            if (!Request.Headers.TryGetValue("X-DBS-Country", out var country))
+            {
+                return BadRequest("Missing X-Custom-Header header.");
+            }
+
+            if(accept!= _configuration.GetValue<string>("CMCPSettings:CMCP_HEADER_ACCEPT"))
+            {
+                return BadRequest("Invalid accept header.");
+            }
+
+            if (req_client_id != _configuration.GetValue<string>("CMCPSettings:CMCP_HEADER_ReqClientId"))
+            {
+                return BadRequest("Invalid CMCP_HEADER_ReqClientId header.");
+            }
+
+            if (country != _configuration.GetValue<string>("CMCPSettings:CMCP_HEADER_Country"))
+            {
+                return BadRequest("Invalid CMCP_HEADER_Country header.");
+            }
+            #endregion
+
+            var card_number = _paylod.query.contactFilter.cardNumber;
+
+            if (string.IsNullOrEmpty(card_number))
+            {
+                return BadRequest("Card number is required header.");
+            }
+
+            // Check if card_number is exactly 16 digits
+            if (!System.Text.RegularExpressions.Regex.IsMatch(card_number, @"^\d{16}$"))
+            {
+                return BadRequest("Card number must be exactly 16 digits.");
+            }
+
+            // call wcf service to search customer profile by card number
+
+            // Generate Random Name
+            var faker = new Faker();
+
+            long unixTimestamp = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+            var helper = new Helper();
+            var customerProfile = new
+            {
+                CardNumber = card_number,
+                Branchcode = "0001",
+                Timestamp = unixTimestamp,
+                Salutation = "Mr",
+                Name = faker.Name.FullName(),
+                NRIC = helper.GenerateSingaporeNRIC(),
+                Mobile = helper.GenerateSingaporePhoneNumber(faker),
+                RMName= helper.GenerateRMName(faker),
+                segment = "PB"
+            };
+            return Ok(customerProfile);
+        }
+    }
+}
---
+++ Models/RequestModel.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace OAuthServer.Models
+{
+    public class ParaCustomerProfile
+    {
+        public Query query { get; set; }
+        public PageForCustProfile page { get; set; }
+    }
+    public class Query
+    {
+        public ContactFilter contactFilter { get; set; }
+    }
+
+    public class PageForCustProfile
+    {
+        public int pageSize { get; set; }
+        public int pageNumber { get; set; }
+    }
+
+    public class ContactFilter
+    {
+        public string contactTypeCode { get; set; }
+        public string contactPhoneNumber { get; set; }
+
+        public string cardNumber { get; set; }
+    }
+}
